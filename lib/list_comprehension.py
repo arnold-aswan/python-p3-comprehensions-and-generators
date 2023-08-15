8e530e179a7a7d63977061a0719cca5c1d201226
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-15 15:39:15 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    numlist = [num for num in num_list if num % 2 == 0]
+    return numlist
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
